0863e07bc97f46407558e84e89f247f8b98c373f
Marek Strnad <93276084+kerblik@users.noreply.github.com>
2023-09-28 10:46:43 +0200

    přidán remove-todo

---
+++ app.js
@@ -0,0 +1,9 @@
+// remove.js
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+for (var i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
+}
